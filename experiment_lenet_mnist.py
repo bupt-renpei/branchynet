477bee3ad0cb2531ed5b173cb0f0d8f94d13258c
bupt-renpei <renpei@bupt.edu.cn>
2018-12-20 17:42:48 +0800

    lenet_mnist GPU version

--- experiment_lenet_mnist.py
+++ experiment_lenet_mnist.py
@@ -12,6 +12,8 @@ from chainer import cuda
 
 from networks import lenet_mnist
 
+print '1. lenet_mnist.get_network()'
+
 branchyNet = lenet_mnist.get_network()
 branchyNet.to_gpu()
 branchyNet.training()
@@ -20,6 +22,8 @@ branchyNet.training()
 # Import Data
 
 # In[4]:
+
+print '2. mnist.get_data()'
 
 from datasets import mnist
 x_train, y_train, x_test, y_test = mnist.get_data()
@@ -41,6 +45,7 @@ TRAIN_NUM_EPOCHS = 50
 main_loss, main_acc, main_time = utils.train(branchyNet, x_train, y_train, main=True, batchsize=TRAIN_BATCHSIZE,
                                              num_epoch=TRAIN_NUM_EPOCHS)
 
+print '3. main_acc : ', main_acc
 
 # Train BranchyNet
 
@@ -50,22 +55,24 @@ TRAIN_NUM_EPOCHS = 100
 branch_loss, branch_acc, branch_time = utils.train(branchyNet, x_train, y_train, batchsize=TRAIN_BATCHSIZE,
                                              num_epoch=TRAIN_NUM_EPOCHS)
 
+print '4. branch_acc : ', branch_acc
+
 #set network to inference mode
-branchyNet.testing()
+# (redundance) branchyNet.testing()
 
 
 # Visualizing Network Training
 
 # In[8]:
 
-visualize.plot_layers(main_loss, xlabel='Epochs', ylabel='Training Loss')
-visualize.plot_layers(main_acc, xlabel='Epochs', ylabel='Training Accuracy')
+# (visualize) visualize.plot_layers(main_loss, xlabel='Epochs', ylabel='Training Loss')
+# (visualize) visualize.plot_layers(main_acc, xlabel='Epochs', ylabel='Training Accuracy')
 
 
 # In[9]:
 
-visualize.plot_layers(zip(*branch_loss), xlabel='Epochs', ylabel='Training Loss')
-visualize.plot_layers(zip(*branch_acc), xlabel='Epochs', ylabel='Training Accuracy')
+# (visualize) visualize.plot_layers(zip(*branch_loss), xlabel='Epochs', ylabel='Training Loss')
+# (visualize) visualize.plot_layers(zip(*branch_acc), xlabel='Epochs', ylabel='Training Accuracy')
 
 
 # Run test suite and visualize
@@ -73,15 +80,22 @@ visualize.plot_layers(zip(*branch_acc), xlabel='Epochs', ylabel='Training Accura
 # In[11]:
 
 #set network to inference mode
+
+print '5. Set network to inference mode'
+
+print '5.1 branchyNet.testing()'
 branchyNet.testing()
 branchyNet.verbose = False
+
 branchyNet.to_gpu()
 g_baseacc, g_basediff, _, _ = utils.test(branchyNet,x_test,y_test,main=True,batchsize=TEST_BATCHSIZE)
 g_basediff = (g_basediff / float(len(y_test))) * 1000.
 
+print '  g_baseacc : ', g_baseacc
+
 #branchyNet.to_cpu()
-c_baseacc, c_basediff, _, _ = utils.test(branchyNet,x_test,y_test,main=True,batchsize=TEST_BATCHSIZE)
-c_basediff = (c_basediff / float(len(y_test))) * 1000.
+# (CPU) c_baseacc, c_basediff, _, _ = utils.test(branchyNet,x_test,y_test,main=True,batchsize=TEST_BATCHSIZE)
+# (CPU) c_basediff = (c_basediff / float(len(y_test))) * 1000.
 
 
 # In[30]:
@@ -94,8 +108,10 @@ thresholds = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25,
 
 #GPU
 branchyNet.to_gpu()
-g_ts, g_accs, g_diffs, g_exits = utils.screen_branchy(branchyNet, x_test, y_test, thresholds,
-                                                    batchsize=TEST_BATCHSIZE, verbose=True)
+g_ts, g_accs, g_diffs, g_exits = utils.screen_branchy(branchyNet, x_test, y_test, thresholds, batchsize=TEST_BATCHSIZE, verbose=True)
+
+print '  g_accs : ', g_accs
+
 # g_ts, g_accs, g_diffs, g_exits = utils.screen_leaky(leakyNet, x_test, y_test, thresholds, inc_amt=-0.1,
 #                                                     batchsize=TEST_BATCHSIZE, verbose=True)
 
@@ -105,47 +121,51 @@ g_diffs *= 1000.
 
 # In[ ]:
 
-visualize.plot_line_tradeoff(g_accs, g_diffs, g_ts, g_exits, g_baseacc, g_basediff, all_samples=False, inc_amt=-0.0001000,
-                             our_label='BranchyLeNet', orig_label='LeNet', xlabel='Runtime (ms)', 
-                             title='LeNet GPU', output_path='_figs/lenet_gpu.pdf')
+# (visualize) visualize.plot_line_tradeoff(g_accs, g_diffs, g_ts, g_exits, g_baseacc, g_basediff, all_samples=False, inc_amt=-0.0001000,
+# (visualize)                              our_label='BranchyLeNet', orig_label='LeNet', xlabel='Runtime (ms)',
+# (visualize)                              title='LeNet GPU', output_path='_figs/lenet_gpu.pdf')
 
 
 # In[32]:
 
 #CPU
-branchyNet.to_cpu()
-c_ts, c_accs, c_diffs, c_exits  = utils.screen_branchy(branchyNet, x_test, y_test, thresholds,
-                                                     batchsize=TEST_BATCHSIZE, verbose=True)
+# (CPU) branchyNet.to_cpu()
+# (CPU) c_ts, c_accs, c_diffs, c_exits  = utils.screen_branchy(branchyNet, x_test, y_test, thresholds,
+# (CPU)                                                      batchsize=TEST_BATCHSIZE, verbose=True)
 # c_ts, c_accs, c_diffs, c_exits  = utils.screen_branchy(branchyNet, x_test, y_test, g_ts, inc_amt=0.01,
 #                                                      batchsize=TEST_BATCHSIZE, prescreen=False, verbose=True)
 #convert to ms
-c_diffs *= 1000.
+# (CPU) c_diffs *= 1000.
 
 
 # In[22]:
 
-visualize.plot_line_tradeoff(c_accs, c_diffs, c_ts, c_exits, c_baseacc, c_basediff, all_samples=False, inc_amt=-0.0001000,
-                             our_label='BranchyLeNet', orig_label='LeNet', xlabel='Runtime (ms)',
-                             title='LeNet CPU', output_path='_figs/lenet_cpu.pdf')
+# (visualize) visualize.plot_line_tradeoff(c_accs, c_diffs, c_ts, c_exits, c_baseacc, c_basediff, all_samples=False, inc_amt=-0.0001000,
+# (visualize)                              our_label='BranchyLeNet', orig_label='LeNet', xlabel='Runtime (ms)',
+# (visualize)                              title='LeNet CPU', output_path='_figs/lenet_cpu.pdf')
 
 
 # In[ ]:
 
 #Compute table results
-utils.branchy_table_results(c_baseacc, c_basediff, g_basediff, c_accs, c_diffs, g_accs, g_diffs, inc_amt=0.000, 
-                          network='LeNet')
+# (CPU) utils.branchy_table_results(c_baseacc, c_basediff, g_basediff, c_accs, c_diffs, g_accs, g_diffs, inc_amt=0.000,
+# (CPU)                           network='LeNet')
+
+print '6. GPU Results:'
+utils.branchy_table_results('LeNet-MNIST', g_baseacc, g_basediff, g_accs, c_diffs, g_exits, g_ts)
 
 
 # Save model/data
 
 # In[40]:
 
+print '7. Save mode'
 import dill
-branchyNet.to_cpu()
+# (CPU) branchyNet.to_cpu()
+branchyNet.to_gpu()
 with open("_models/lenet_mnist.bn", "w") as f:
     dill.dump(branchyNet, f)
 with open("_models/lenet_mnist_gpu_results.pkl", "w") as f:
     dill.dump({'accs': g_accs, 'rt': g_diffs, 'exits': g_exits, 'ts': g_ts, 'baseacc': g_baseacc, 'basediff': g_basediff}, f)
-with open("_models/lenet_mnist_cpu_results.pkl", "w") as f:
-    dill.dump({'accs': c_accs, 'rt': c_diffs, 'exits': c_exits, 'ts': c_ts, 'baseacc': c_baseacc, 'basediff': c_basediff}, f)
-
+# (CPU) with open("_models/lenet_mnist_cpu_results.pkl", "w") as f:
+# (CPU)     dill.dump({'accs': c_accs, 'rt': c_diffs, 'exits': c_exits, 'ts': c_ts, 'baseacc': c_baseacc, 'basediff': c_basediff}, f)
---
+++ readPKL.py
@@ -0,0 +1,4 @@
+import cPickle as pickle
+f = open('path')
+info = pickle.load(f)
+print info
