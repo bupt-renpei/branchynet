3d58928055a46bbfd5f49562e7279c644074d224
bupt-renpei <renpei@bupt.edu.cn>
2018-12-20 21:07:39 +0800

    GPU version

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mkdir -p datasets/data/pcifar10
-cd datasets/data/pcifar10
+#mkdir -p datasets/data/pcifar10
+#cd datasets/data/pcifar10
 # or download from here https://drive.google.com/file/d/0Byyuc5LmNmJPWUc5dVdUSms3U1E/view?usp=sharing
-wget 'https://dl.dropboxusercontent.com/u/4542002/branchynet/data.npz' -O data.npz
+#wget 'https://dl.dropboxusercontent.com/u/4542002/branchynet/data.npz' -O data.npz
+
+cp ../data.npz /home/acer/branchynet/datasets/data/pcifar10
