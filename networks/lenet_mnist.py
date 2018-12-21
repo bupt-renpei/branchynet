db683768d9ee3a6a162540cd3ebee39bf72107fc
bupt-renpei <renpei@bupt.edu.cn>
2018-12-21 08:17:13 +0800

    lenet_mnist None

--- networks/lenet_mnist.py
+++ networks/lenet_mnist.py
@@ -9,18 +9,23 @@ import chainer.functions as F
 def get_network(percentTrainKeeps=1):
     network = [
         L.Convolution2D(1, 5, 5,stride=1, pad=3),
-        Branch([FL(F.max_pooling_2d, 2, 2), FL(F.ReLU()), L.Convolution2D(5, 10,  3, pad=1, stride=1),
-              FL(F.max_pooling_2d, 2, 2), FL(F.ReLU()), L.Linear(640, 10)]),
+        # (Branch) Branch([FL(F.max_pooling_2d, 2, 2), FL(F.ReLU()), L.Convolution2D(5, 10,  3, pad=1, stride=1), FL(F.max_pooling_2d, 2, 2), FL(F.ReLU()), L.Linear(640, 10)]),
+        Branch([L.Linear(None, 10)]),
         FL(F.max_pooling_2d, 2, 2),
         FL(F.ReLU()),
+        Branch([L.Linear(None, 10)]),
         L.Convolution2D(5, 10, 5,stride=1, pad=3),
+        Branch([L.Linear(None, 10)]),
         FL(F.max_pooling_2d, 2, 2),
         FL(F.ReLU()),
+        Branch([L.Linear(None, 10)]),
         L.Convolution2D(10, 20, 5,stride=1, pad=3),
+        Branch([L.Linear(None, 10)]),
         FL(F.max_pooling_2d, 2, 2),
         FL(F.ReLU()),
-        L.Linear(720, 84),
-        Branch([L.Linear(84, 10)])
+        Branch([L.Linear(None, 10)]),
+        L.Linear(None, 84),  # 720, 84
+        Branch([L.Linear(None, 10)])  # 84, 10
     ]
     net = BranchyNet(network, percentTrainKeeps=percentTrainKeeps)
     return net
